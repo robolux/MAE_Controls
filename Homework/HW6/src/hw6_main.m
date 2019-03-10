ea95e38fdf8b40e6546e01e5fa19deaa766d3259
robolux <HyperDrive@robolux.local>
2019-03-10 13:49:47 -0500

    hw6 fluidity update

--- Homework/HW6/src/hw6_main.m
+++ Homework/HW6/src/hw6_main.m
@@ -1,9 +1,10 @@
 % Hunter Phillips
 % Homework 6
 % MAE 488
-% 03/02/19
+% 03/10/19
 
 clc
+clear
 format compact
 
 %% Header
@@ -58,12 +59,12 @@ C_s_R_s = sys4
 sys5=-feedback(sys2,sys1);
 X_s_G_s = sys5
 
-%% Problem 23M
+%% Problem 23M (b)
 
 su_bullets = repmat('*', 25, 1);
 fprintf('\n\n')
 fprintf('%c',su_bullets)
-fprintf('\nProblem 23M\n')
+fprintf('\nProblem 23M (b)\n')
 fprintf('%c',su_bullets)
 fprintf('\n\n')
 
@@ -78,12 +79,12 @@ fprintf('X_1(s)/U_2(s) is from input 2 to output\n')
 
 transfer_fun = tf(sys)
 
-%% Problem 25M
+%% Problem 25M (b)
 
 su_bullets = repmat('*', 25, 1);
 fprintf('\n\n')
 fprintf('%c',su_bullets)
-fprintf('\nProblem 25M\n')
+fprintf('\nProblem 25M (b)\n')
 fprintf('%c',su_bullets)
 fprintf('\n\n')
 
@@ -97,6 +98,7 @@ fprintf('  y  = %.1f*z_1 + %.1f*z_2\n', sys2.C(1), sys2.C(2))
 
 %% Problem 35M
 
+f1 = figure(1);
 su_bullets = repmat('*', 25, 1);
 fprintf('\n\n')
 fprintf('%c',su_bullets)
@@ -111,7 +113,12 @@ ylabel('v (ft/sec)')
 title({'MAE 488, Homework 6, Problem 6.35M'},'interpreter','latex')
 ylim([0 275])
 
+fprintf('Results Plotted in Figure 1\n')
+% print(f1,'..\results\problem_5_35M.png','-dpng','-r1200');
+
 %% Problem 41S
+
+f2 = figure(2);
 
 su_bullets = repmat('*', 25, 1);
 fprintf('\n\n')
@@ -127,7 +134,12 @@ title({'MAE 488, Homework 6, Problem 6.41S'},'interpreter','latex')
 xlabel('t')
 ylabel('y(t)')
 
+fprintf('Results Plotted in Figure 2\n')
+% print(f2,'..\results\problem_5_41S.png','-dpng','-r1200');
+
 %% Problem 45S
+
+f3 = figure(3);
 
 su_bullets = repmat('*', 25, 1);
 fprintf('\n\n')
@@ -149,8 +161,13 @@ xlabel('t')
 legend('y_1(t)','y_2(t)','location','southeast','fontsize',16)
 ylim([-2 3])
 
+fprintf('\nResults Plotted in Figure 3\n')
+% print(f3,'..\results\problem_5_45S.png','-dpng','-r1200');
+
 
 %% Problem 46S
+
+f4 = figure(4);
 
 su_bullets = repmat('*', 25, 1);
 fprintf('\n\n')
@@ -164,19 +181,22 @@ B_45s = [0;0]
 C_45s = [1, 3; 0, 1]
 D_45s = [0;0]
 
-% Going to need 1/s integrator combined with 45 without source
-
 sim('problem_46s')
 
 plot(simout.time, simout.data)
 title({'MAE 488, Homework 6, Problem 6.46S'},'interpreter','latex')
 xlabel('t')
-legend('y_1(t)','y_2(t)','location','southeast','fontsize',16)
-ylim([-2 3])
+legend('y_1(t)','y_2(t)','location','northeast','fontsize',16)
+ylim([-1 15])
+grid
+
+fprintf('\nResults Plotted in Figure 4\n')
+% print(f4,'..\results\problem_5_46S.png','-dpng','-r1200');
 
 %% Problem 28M
 
 format long
+f5 = figure(5);
 
 su_bullets = repmat('*', 25, 1);
 fprintf('\n\n')
@@ -198,14 +218,31 @@ B = [0; 0; 0; k2/m2];
 C = [1, 0, 0, 0; 0, 0, 1, 0]; 
 D = [0; 0];
 
+fprintf('Part A\n-------\n')
 sys = ss(A,B,C,D)
 
 % part b
+fprintf('\nPart B\n-------\n\n')
+fprintf('Results Plotted in Figure 5\n\n')
 impulse(sys)
+title({'MAE 488, Homework 6, Problem 5.28M'},'interpreter','latex')
+
 
 % part c
-poly(A)
-eig(A)
+fprintf('\nPart C\n-------\n\n')
+% print(f5,'..\results\problem_5_28M.png','-dpng','-r1200');
+
+% characteristic polynomial
+po = poly(A);
+fprintf('Characteristic Polynomial:\n')
+fprintf('%.2fs^4 + %.2fs^3 + %.2fs^2 + %.2fs^1 + %.2f = 0\n\n'...
+        ,po(1), po(2), po(3), po(4), po(5))
+
+% roots
+roots = eig(A)
 
 % part d
-tfsys = tf(sys)
+fprintf('\n\nPart D\n-------\n\n')
+tfsys = tf(sys);
+X1_s_Y_s = tfsys(1)
+X2_s_Y_s = tfsys(2)
